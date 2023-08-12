bc42ea94270ddbbbfe446a5a9365b3355909aadb
antonicr1986 <antonicr1986@hotmail.com>
2023-08-12 15:12:04 +0200

    Agregado en Votante.cs propiedad Apellidos y constructores que usaremos en un futuro

--- VotoElectronico/Votante.cs
+++ VotoElectronico/Votante.cs
@@ -8,8 +8,21 @@ namespace VotoElectronico
 {
     public class Votante
     {
-        public string Nombre { get; set; }  
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
         public int Edad { get; set; }   
-        public bool Antecedentes { get; set; }  
+        public bool Antecedentes { get; set; }
+
+        public Votante()
+        {
+        }
+
+        public Votante(string nombre, string apellidos, int edad, bool antecedentes)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+            Edad = edad;
+            Antecedentes = antecedentes;
+        }
     }
 }
